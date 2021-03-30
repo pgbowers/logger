e13d07bde11d36eb8c6dbc9e72773a6f2a5e0606
pgbowers <pgbowers@gmail.com>
2021-03-30 07:33:05 -0300

    Changed layout to one row

--- logger.py
+++ logger.py
@@ -5,9 +5,7 @@ sg.theme('Kayak')
 counties = ('Annapolis', 'Digby', 'Hants', 'Inverness')
 
 layout = [[sg.T('NSARA Contest Logger')],
-        [sg.T('Call:'), sg.I(k = '-Call-'), sg.T('Time:'), sg.I(k = '-Time-')],
-        [sg.T('RST:'), sg.I(k = '-RST-'), sg.T('Mode:'), sg.Listbox(values = ('Phone', 'CW', 'Digital'), default_values = 'Phone', select_mode = 'LISTBOX_SELECT_MODE_SINGLE', bind_return_key = True, size = (15, 1), k = '-Mode-')],
-        [sg.T('County or Serial:'), sg.Listbox(values = counties, k = '-County-')],
+        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(size = (10, 1), k = '-Time-'), sg.T('RST:'), sg.I(size = (10, 1), k = '-RST-'), sg.T('Mode:'), sg.Listbox(values = ('Phone', 'CW', 'Digital'), default_values = 'Phone', select_mode = 'LISTBOX_SELECT_MODE_SINGLE', bind_return_key = True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Listbox(values = counties, k = '-County-')],
         [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1)), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1))]]  
 
 window = sg.Window('Logger', layout) 
