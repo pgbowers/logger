ba4477c5e31727677ab6bbb63e2c3d603ce1ff78
pgbowers <pgbowers@gmail.com>
2021-04-02 14:14:09 -0300

    Apr2 - working ok

--- logger.py
+++ logger.py
@@ -4,20 +4,22 @@ sg.theme('Kayak')
 
 def clearInput():
         # A list of the inputs we want to clear
-       keys_to_clear = ['-Call-', '-Time-', '-RST-']
+       keys_to_clear = ['-Call-', '-Time-', '-RST-', '-County-']
        # This section will clear the inputs and restore defaults
        for key in keys_to_clear:
         window[key]('')
         window['-RST-']('59')
-        window['-Mode-'](modes)
-        window['-County-'](counties)
+        window['-Mode-']('Phone')
+        window['-County-']('')
 
-
-counties = ['Annapolis', 'Digby', 'Hants', 'Inverness']
+counties = ['Annapolis', 'Antigonish', 'Cape Breton', 'Colchester', 'Cumberland', 
+            'Digby', 'Guysborough','Halifax', 'Hants', 'Inverness', 'Kings', 'Lunenburg', 
+            'Pictou', 'Queens', 'Richmond', 'Shelburne', 'Victoria', 'Yarmouth']
 modes = ['Phone', 'CW', 'Digital']
 
 layout = [[sg.T('NSARA Contest Logger')],
-        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(size = (10, 1), k = '-Time-'), sg.T('RST:'), sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Listbox(values = modes, default_values = 'Phone', select_mode = 'LISTBOX_SELECT_MODE_SINGLE', bind_return_key = True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Listbox(values = counties, k = '-County-')],
+        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(size = (10, 1), k = '-Time-'), sg.T('RST:'), 
+        sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Combo(values = (modes), default_value = 'Phone', enable_events=True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Combo(values = counties, k = '-County-')],
         [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1)), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1)), sg.B('Exit', tooltip = 'Exit the program', size = (15, 1))]]  
 
 window = sg.Window('Logger', layout) 
@@ -30,16 +32,21 @@ while True:
     if event == 'Clear':
         clearInput()
     if event == 'Save':
-        # Write the QSO to a local file        
-        with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/mylog1.csv", "a") as log:
-            log.write(values['-Call-'] + ',')
-            log.write(values['-Time-'] + ',')
-            log.write(values['-RST-'] + ',')
-            log.write(values['-Mode-'][0] + ',')
-            log.write(values['-County-'][0])
-           
-        clearInput()
-
+        if values['-Call-'] != '' and values['-County-'] != '':        
+            # Write the QSO to a local file        
+            with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/mylog1.csv", "a") as log:
+                log.write(values['-Call-'] + ',')
+                log.write(values['-Time-'] + ',')
+                log.write(values['-RST-'] + ',')
+                log.write(values['-Mode-'] + ',')
+                log.write(values['-County-'])
+            clearInput()
+        # Check for empty input
+        elif values['-Call-']=='': 
+            sg.popup('Callsign cannot be blank.', title = 'OOPS!') 
+        elif values['-County-']== '':
+            sg.popup('County or Serial cannot be blank.', title = 'OOPS!')       
+            
 window.close()
 #with open("/home/pi/apps/Projects/PySimpleGUI/Temp/cpu_temp1.csv", "a") as log:        
     #    log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(cel_temp)))
