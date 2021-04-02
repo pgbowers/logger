503f8046dedea7eb6dc286a60c2bd31ed8b37922
pgbowers <pgbowers@gmail.com>
2021-04-02 18:56:27 -0300

    Apr 2 at 6PM

--- To-Do_logger.md
+++ To-Do_logger.md
@@ -5,9 +5,9 @@ March 25, 2021
 * [ ] Set Time field to systime when Call field is filled in
 * [x] RST should default to 59 but be changeable
 * [x] Do dropdown for Mode: Phone, CW, Digital
-* [ ] Do dropdown for County listing all NS counties but allow other input as well
+* [x] Do dropdown for County listing all NS counties but allow other input as well
 * [ ] Check for dupes
-* [ ] Show stats: # of QSO's + # of counties = score
+* [x] Show stats: # of QSO's + # of counties = score
 * [ ] Put fields into columns
 * [ ] Put a frame around the input section
 * [ ] Maybe display some number of recent entries below the input form and buttons
--- logger.py
+++ logger.py
@@ -17,11 +17,17 @@ counties = ['Annapolis', 'Antigonish', 'Cape Breton', 'Colchester', 'Cumberland'
             'Pictou', 'Queens', 'Richmond', 'Shelburne', 'Victoria', 'Yarmouth']
 modes = ['Phone', 'CW', 'Digital']
 
+QSOCount = 0
+countiesWorked = []
+score = 0
+
 layout = [[sg.T('NSARA Contest Logger')],
         [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(size = (10, 1), k = '-Time-'), sg.T('RST:'), 
         sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Combo(values = (modes), default_value = 'Phone', enable_events=True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Combo(values = counties, k = '-County-')],
-        [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1)), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1)), sg.B('Exit', tooltip = 'Exit the program', size = (15, 1))]]  
-
+        [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1)), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1)), sg.B('Exit', tooltip = 'Exit the program', size = (15, 1))],
+        [sg.T("QSO's: "), sg.T('', size = (5, 1), k = '-QSO-'), sg.T("Counties: "), sg.T('', size = (5, 1),k = '-Counties-'), sg.T("Score: "), sg.T('', size = (5, 1),k = '-Score-')
+        ] ]   
+          
 window = sg.Window('Logger', layout) 
 
 while True:
@@ -32,7 +38,8 @@ while True:
     if event == 'Clear':
         clearInput()
     if event == 'Save':
-        if values['-Call-'] != '' and values['-County-'] != '':        
+        if values['-Call-'] != '' and values['-County-'] != '': 
+            #*****************       
             # Write the QSO to a local file        
             with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/mylog1.csv", "a") as log:
                 log.write(values['-Call-'] + ',')
@@ -41,12 +48,24 @@ while True:
                 log.write(values['-Mode-'] + ',')
                 log.write(values['-County-'])
             clearInput()
+
+            #****************
+            # Display a runing total of QSO's, new counties worked and total scoreS
+            QSOCount+=1
+            window.Element('-QSO-').update(QSOCount)
+            if not values['-County-'] in countiesWorked:
+                countiesWorked.append(values['-County-'])                
+            window.Element('-Counties-').update(len(countiesWorked))
+            score = QSOCount * len(countiesWorked)
+            window.Element('-Score-').update(score)
+            #***************
+        #**************
         # Check for empty input
         elif values['-Call-']=='': 
             sg.popup('Callsign cannot be blank.', title = 'OOPS!') 
         elif values['-County-']== '':
-            sg.popup('County or Serial cannot be blank.', title = 'OOPS!')       
-            
+            sg.popup('County or Serial cannot be blank.', title = 'OOPS!') 
+        #***************            
 window.close()
 #with open("/home/pi/apps/Projects/PySimpleGUI/Temp/cpu_temp1.csv", "a") as log:        
     #    log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(cel_temp)))
