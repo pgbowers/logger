5cb73a4f7ca89691ce7113ad63501ab20f62f473
pgbowers <pgbowers@gmail.com>
2021-04-02 07:47:41 -0300

    Apr2 - Save to file working

--- logger.py
+++ logger.py
@@ -13,28 +13,32 @@ def clearInput():
         window['-County-'](counties)
 
 
-counties = ('Annapolis', 'Digby', 'Hants', 'Inverness')
-modes = ('Phone', 'CW', 'Digital')
+counties = ['Annapolis', 'Digby', 'Hants', 'Inverness']
+modes = ['Phone', 'CW', 'Digital']
 
 layout = [[sg.T('NSARA Contest Logger')],
-        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(size = (10, 1), k = '-Time-'), sg.T('RST:'), sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Listbox(values = (modes), default_values = 'Phone', select_mode = 'LISTBOX_SELECT_MODE_SINGLE', bind_return_key = True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Listbox(values = counties, k = '-County-')],
+        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(size = (10, 1), k = '-Time-'), sg.T('RST:'), sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Listbox(values = modes, default_values = 'Phone', select_mode = 'LISTBOX_SELECT_MODE_SINGLE', bind_return_key = True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Listbox(values = counties, k = '-County-')],
         [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1)), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1)), sg.B('Exit', tooltip = 'Exit the program', size = (15, 1))]]  
 
 window = sg.Window('Logger', layout) 
 
 while True:
-    event, values = window.read()
-    for i in values:
-        print(values[i])
+    event, values = window.read() 
+  
     if event == 'Exit' or event == sg.WIN_CLOSED:
         break
     if event == 'Clear':
         clearInput()
     if event == 'Save':
+        # Write the QSO to a local file        
         with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/mylog1.csv", "a") as log:
-                for item in values:
-                        log.write(str(values[item]) + ',')
-                clearInput()
+            log.write(values['-Call-'] + ',')
+            log.write(values['-Time-'] + ',')
+            log.write(values['-RST-'] + ',')
+            log.write(values['-Mode-'][0] + ',')
+            log.write(values['-County-'][0])
+           
+        clearInput()
 
 window.close()
 #with open("/home/pi/apps/Projects/PySimpleGUI/Temp/cpu_temp1.csv", "a") as log:        
