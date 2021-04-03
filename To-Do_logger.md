204baddefb9e4aa9849bba5ec54de6a16d330fab
pgbowers <pgbowers@gmail.com>
2021-04-03 08:06:48 -0300

    Apr3 at 8 am

--- To-Do_logger.md
+++ To-Do_logger.md
@@ -2,7 +2,7 @@ March 25, 2021
 * [ ] Do a menu layout
 * [ ] Center the NSARA title
 * [ ] Remove the top titlebar
-* [ ] Set Time field to systime when Call field is filled in
+* [x] Set Time field to systime when Call field is filled in
 * [x] RST should default to 59 but be changeable
 * [x] Do dropdown for Mode: Phone, CW, Digital
 * [x] Do dropdown for County listing all NS counties but allow other input as well
@@ -12,3 +12,6 @@ March 25, 2021
 * [ ] Put a frame around the input section
 * [ ] Maybe display some number of recent entries below the input form and buttons
 * [ ] Maybe select a previously saved entry and allow changes?
+* [x] countiesWorked list should ignore serial numbers
+* [ ] Make a Clean Sweep popup for all 18 counties
+* [ ] Save and restore scores between sessionsS
--- logger.py
+++ logger.py
@@ -1,13 +1,18 @@
 import PySimpleGUI as sg
+from datetime import datetime
 
 sg.theme('Kayak')
 
+now = datetime.now()
+current_time = now.strftime("%H:%M")
+
 def clearInput():
         # A list of the inputs we want to clear
-       keys_to_clear = ['-Call-', '-Time-', '-RST-', '-County-']
+       keys_to_clear = ['-Call-', '-RST-', '-County-']
        # This section will clear the inputs and restore defaults
        for key in keys_to_clear:
         window[key]('')
+        #window['-Time-']('')
         window['-RST-']('59')
         window['-Mode-']('Phone')
         window['-County-']('')
@@ -22,7 +27,7 @@ countiesWorked = []
 score = 0
 
 layout = [[sg.T('NSARA Contest Logger')],
-        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(size = (10, 1), k = '-Time-'), sg.T('RST:'), 
+        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(default_text = current_time, size = (10, 1), k = '-Time-'), sg.T('RST:'), 
         sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Combo(values = (modes), default_value = 'Phone', enable_events=True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Combo(values = counties, k = '-County-')],
         [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1)), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1)), sg.B('Exit', tooltip = 'Exit the program', size = (15, 1))],
         [sg.T("QSO's: "), sg.T('', size = (5, 1), k = '-QSO-'), sg.T("Counties: "), sg.T('', size = (5, 1),k = '-Counties-'), sg.T("Score: "), sg.T('', size = (5, 1),k = '-Score-')
@@ -34,6 +39,11 @@ while True:
     event, values = window.read() 
   
     if event == 'Exit' or event == sg.WIN_CLOSED:
+        # Write the scores to a local file for next session      
+        with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/scores.csv", "a") as scores:
+            scores.write(str(QSOCount) + ',')
+            scores.write(str(len(countiesWorked)) + ',')
+            scores.write(str(score) + '\n')
         break
     if event == 'Clear':
         clearInput()
@@ -43,22 +53,23 @@ while True:
             # Write the QSO to a local file        
             with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/mylog1.csv", "a") as log:
                 log.write(values['-Call-'] + ',')
-                log.write(values['-Time-'] + ',')
+                log.write(current_time + ',')
+                #log.write(values['-Time-'] + ',')
                 log.write(values['-RST-'] + ',')
                 log.write(values['-Mode-'] + ',')
-                log.write(values['-County-'])
-            clearInput()
-
+                log.write(values['-County-'] + '\n')          
             #****************
-            # Display a runing total of QSO's, new counties worked and total scoreS
+            # Display a runing total of QSO's, new counties worked and total score
             QSOCount+=1
             window.Element('-QSO-').update(QSOCount)
-            if not values['-County-'] in countiesWorked:
+            if not values['-County-'] in countiesWorked and values['-County-'] in counties:
                 countiesWorked.append(values['-County-'])                
             window.Element('-Counties-').update(len(countiesWorked))
             score = QSOCount * len(countiesWorked)
             window.Element('-Score-').update(score)
             #***************
+            window.Element('-Time-').update(current_time)            
+            clearInput()
         #**************
         # Check for empty input
         elif values['-Call-']=='': 
