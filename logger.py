9670116b3236c6e0f5804f22a4c8fb253651894a
pgbowers <pgbowers@gmail.com>
2021-04-04 15:13:39 -0300

    Apr 4 3PM - dupe check not working

--- logger.py
+++ logger.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 sg.theme('Kayak')
 
+# Get the current system time for the log
 now = datetime.now()
 current_time = now.strftime("%H:%M")
 
@@ -25,16 +26,18 @@ modes = ['Phone', 'CW', 'Digital']
 QSOCount = 0
 countiesWorked = []
 score = 0
+# A dictionary of calls and modes so we can check for duplicates.
+#callDict = {}
 callList = []
 
 layout = [[sg.T('NSARA Contest Logger', size = (60,2), justification='right')],
-        [sg.T('Call:'), sg.I(size = (10, 1), k = '-Call-'), sg.T('Time:'), sg.I(default_text = current_time, size = (10, 1), k = '-Time-'), sg.T('RST:'), 
-        sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Combo(values = (modes), default_value = 'Phone', enable_events=True, size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Combo(values = counties, size = (15,1), k = '-County-')],
+        [sg.T('Call:'), sg.I(size = (10, 1), focus = False, k = '-Call-'), sg.T('Time:'), sg.I(default_text = current_time, size = (10, 1), k = '-Time-'), sg.T('RST:'), 
+        sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Combo(values = (modes), default_value = 'Phone', size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Combo(values = counties, size = (15,1), k = '-County-')],
         [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1)), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1)), sg.B('Exit', tooltip = 'Exit the program', size = (15, 1))],
         [sg.T("QSO's: "), sg.T('', size = (5, 1), k = '-QSO-'), sg.T("Counties: "), sg.T('', size = (5, 1),k = '-Counties-'), sg.T("Score: "), sg.T('', size = (5, 1),k = '-Score-')],
         [sg.Multiline(default_text='Hello list!!', background_color = 'white')]]   
           
-window = sg.Window('Logger', layout, grab_anywhere = True, no_titlebar=False) 
+window = sg.Window('Logger', layout, grab_anywhere = True) 
 
 while True:
     event, values = window.read() 
@@ -45,21 +48,22 @@ while True:
             scores.write(str(QSOCount) + ',')
             scores.write(str(len(countiesWorked)) + ',')
             scores.write(str(score) + '\n')
+            #scores.write('Test123')
         break
     if event == 'Clear':
         clearInput()
+
     if event == 'Save':
-        if not values['-Call-'] in callList and values['-Call-'] != '' and values['-County-'] != '': 
+        if values['-Call-'] != '' and values['-County-'] != '': 
             
-            #if not values['-Call-'] in callList:
-            callList.append(values['-Call-'])
-            print(callList)
+            if not values['-Call-'] in callList:
+                callList.append(values['-Call-'])
+                print(callList)
             #*****************       
             # Write the QSO to a local file        
             with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/mylog1.csv", "a") as log:
                 log.write(values['-Call-'] + ',')
-                log.write(current_time + ',')
-                #log.write(values['-Time-'] + ',')
+                log.write(current_time + ',')                
                 log.write(values['-RST-'] + ',')
                 log.write(values['-Mode-'] + ',')
                 log.write(values['-County-'] + '\n')          
@@ -78,16 +82,13 @@ while True:
             #**************
         # Check for duplicate call signs
         elif values['-Call-'] in callList:
-            dupeChoice = sg.popup_yes_no("Possible Dupe depending on Mode!\n Continue - Yes, No - Clear", title = 'Possible Duplicate Callsign')
-            if dupeChoice == 'No':
-                clearInput()
+            print(callList)
+            sg.popup('Possible Dupe depending on Mode!', background_color='yellow')
+            
         # Check for empty input
         elif values['-Call-']=='': 
-            sg.popup('Callsign cannot be blank.', title = 'OOPS!') 
-        elif values['-County-']== '':
-            sg.popup('County or Serial cannot be blank.', title = 'OOPS!') 
+            sg.popup('Callsign cannot be blank.') 
+        else: # values['-County-']== '':
+            sg.popup('County or Serial cannot be blank.') 
         #***************            
 window.close()
-#with open("/home/pi/apps/Projects/PySimpleGUI/Temp/cpu_temp1.csv", "a") as log:        
-    #    log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(cel_temp)))
-
