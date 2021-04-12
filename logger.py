2cde42b109bfd8a386d1ef14d1a6db2fd0820206
pgbowers <pgbowers@gmail.com>
2021-04-12 16:06:07 -0300

    Apr 12 - working on string formatting

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import pickle
 from datetime import datetime
 
 sg.theme('Kayak')
@@ -12,6 +13,10 @@ countyScore = 0
 # Get the current system time for the log
 now = datetime.now()
 current_time = now.strftime("%H:%M")
+
+# load our previously saved data
+with open('callFile', 'rb') as f:
+    myCall = pickle.load(f)
 
 def clearInput():
         # A list of the inputs we want to clear
@@ -50,27 +55,27 @@ counties = ['Annapolis', 'Antigonish', 'Cape Breton', 'Colchester', 'Cumberland'
             'Pictou', 'Queens', 'Richmond', 'Shelburne', 'Victoria', 'Yarmouth']
 modes = ['Phone', 'CW', 'Digital']
 
-
 # A dictionary of calls and modes so we can check for duplicates.
 #callDict = {}
 callList = []
 
 # ------ Menu Definition ------ #
-menu_def = [['&File', ['&Open     Ctrl-O', '&Save       Ctrl-S', '&Clear Scores', 'E&xit']],
-            ['&Edit', ['&Paste', ['Special', 'Normal', ], 'Undo'], ],
-            ['&Toolbar', ['---', 'Command &1', 'Command &2',
-                        '---', 'Command &3', 'Command &4']],
-            ['&Help', '&About...'], ]
+menu_def = [['&File', ['&Setup', ['Your Callsign', 'Contest Date', ], '&Clear Scores', 'E&xit']],
+            ['&Edit', ['&Paste', ['Special', 'Normal', ], 'Undo'], ],            
+            ['&Help', '&About...'] 
+           ]
 # ------------------------------#
+
 layout =[
         [sg.Menu(menu_def, tearoff=False, pad=(200, 1))],
         [sg.Frame(layout = [[sg.T('Call:'), sg.I(size = (10, 1), focus = False, k = '-Call-'), sg.T('Time:'), sg.I(default_text = current_time, size = (10, 1), k = '-Time-'), sg.T('RST:'),
         sg.I(size = (10, 1), default_text = '59', k = '-RST-'), sg.T('Mode:'), sg.Combo(values = (modes), default_value = 'Phone', size = (10, 1), k = '-Mode-'), sg.T('County or Serial:'), sg.Combo(values = counties, size = (15,1), k = '-County-')],
         [sg.B('Save', tooltip = 'Save this QSO to the log', size = (15,1), pad = ((80, 0),(20,20))), sg.B('Clear', tooltip = 'Clear all fields', size = (15,1), pad = (80, 0)), sg.B('Exit', tooltip = 'Exit the logger', size = (15, 1), pad = (80, 1))]],title = "Input", pad = ((20, 20),(20, 20)))],
         [sg.Frame(layout = [[sg.T("QSO's: "), sg.T('', size = (5, 1), k = '-QSO-'), sg.T("Counties: "), sg.T('', size = (5, 1),k = '-Counties-'), sg.T("Score: "), sg.T('', size = (5, 1),k = '-Score-')]], title = 'Score', pad = ((20, 20),(0, 20)))],
-        [sg.Frame(layout = [[sg.Multiline(default_text='Hello list!!', background_color = 'white')]], title = 'Your Log', pad = ((20, 20),(0, 20)))]] 
-          
-window = sg.Window('NSARA Contest Logger', layout, font = 'Arial', element_justification = 'l', grab_anywhere = True, resizable = True) 
+        [sg.Frame(layout = [[sg.Multiline(default_text='', k = '-LogDisplay-', autoscroll = True, disabled = True, do_not_clear = True, size = (200, 4), background_color = 'white')]], title = 'Your Log', pad = ((20, 20),(0, 20)))]
+        ] 
+        
+window = sg.Window('NSARA Contest Logger for: ' + str(myCall), layout, font = 'Arial', element_justification = 'l', grab_anywhere = True, resizable = True) 
  
 while True:
     event, values = window.read()                                                                                              
@@ -93,23 +98,61 @@ while True:
             #window.reappear() # uses alpha channel so doesn't work on Elementary or PI
 
     if event == 'Open':
-            filename = sg.popup_get_file('file to open', no_window=True)
-            print('Open clicked')
-    
+        filename = sg.popup_get_file('file to open', no_window=True)
+        print('Open clicked')
+
+    if event == 'Your Callsign':
+        callSign = sg.PopupGetText('Enter your Callsign or SWL for shortwave listener', title = 'Your Callsign')
+        with open('callFile', 'wb') as f:
+            pickle.dump(callSign, f)
+        print(callSign)
+
+    if event == 'Clear Scores':        
+        clearScores()   
+    #**************
+    # when the save button is pressed
     if event == 'Save':
+        # make sure that the call and county fields have a value in them
         if values['-Call-'] != '' and values['-County-'] != '': 
-            
+            #******************
+            # build a list of the counties that have been worked
             if not values['-Call-'] in callList:
                 callList.append(values['-Call-'])
-                print(callList)
-            #*****************       
-            # Write the QSO to a local file        
-            with open("/home/user1/apps/python/PySimpleGUI/Projects/logger/mylog1.csv", "a") as log:
-                log.write(values['-Call-'] + ',')
-                log.write(current_time + ',')                
-                log.write(values['-RST-'] + ',')
-                log.write(values['-Mode-'] + ',')
-                log.write(values['-County-'] + '\n')          
+                #print('Line 118: ' + str(callList))
+            #*****************  
+             # build a list of the values entered
+                logentry = []
+                logentry.append(values['-Call-'])
+                logentry.append(current_time)
+                logentry.append(values['-RST-'])
+                logentry.append(values['-Mode-'])
+                logentry.append(values['-County-'])
+                print('Line 135: ' + str(logentry))
+
+                #[print(*word) for word in logentry]
+                #for word in logentry:
+                #    window['-LogDisplay-'].update(value = word, append = False, autoscroll = True)
+
+                for word in logentry:
+                   print (word, end = ' ')
+
+            #*****************    
+            # Write the logentry to a local file        
+            with open('mylog.txt', 'ab') as log:            
+                pickle.dump(logentry , log)
+            #*****************
+
+            with open('mylog.txt', 'rb') as log:
+                readout = []
+                while True:
+                    try:
+                        readout.append(pickle.load(log))
+                        #window['-LogDisplay-'].update(value = readout, append = False, autoscroll = True)
+                        #print(readout)
+                    except EOFError: # reached the end of the file
+                        break                               
+                #print(readout)               
+                                       
             #****************
             # Display a runing total of QSO's, new counties worked and total score
             QSOCount+=1            
@@ -119,7 +162,11 @@ while True:
                 countyScore = len(countiesWorked) 
                 #print('Line 114: ' + str(countyScore))              
             window.Element('-Counties-').update(countyScore)
-            score = QSOCount * countyScore
+            # if the first log entry is a serial number not a county(multiply by zero)
+            if countyScore == 0:
+                score = QSOCount + countyScore
+            else:
+                score = QSOCount * countyScore
             window.Element('-Score-').update(score)
             #***************
             window.Element('-Time-').update(current_time)            
@@ -136,7 +183,5 @@ while True:
         else: # values['-County-']== '':
             sg.popup('County or Serial cannot be blank.') 
         #***************   
-    if event == 'Clear Scores':
-        print('Line 125')
-        clearScores()         
+          
 window.close()
